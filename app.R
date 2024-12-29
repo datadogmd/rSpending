156c3a87da0b2ae1a5c1709ea0f67edc36d5c140
datadogmd <mdusaire50@gmail.com>
2024-12-29 11:27:23 -0600

    added some data intput elements from Shiny Components examples

--- app.R
+++ app.R
@@ -7,33 +7,89 @@
 #    https://shiny.posit.co/
 #
 
+library(bslib)
+library(ggplot2)
+library(lubridate)
 library(shiny)
 
+
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- page_fluid(
+  page_sidebar(
+  title = "Example dashboard",
+  sidebar = sidebar(
+    
+    # choose file input
+    
+    fileInput("file1", "Choose a File"),
+    verbatimTextOutput("file1_contents"),
+    
+    # select variable input
+    
+    varSelectInput("var", "Select variable", mtcars),
+    
+    # date range input
+    
+    dateRangeInput(inputId = "date", label = "Date Range"),
+    hr(),
+    verbatimTextOutput("date"),
+    verbatimTextOutput("date1"),
+    verbatimTextOutput("date2"),
+    
+    verbatimTextOutput("date1_class"),
+    verbatimTextOutput("date1_year")
+  ),
+  card(
+    full_screen = TRUE,
+    card_header("My plot"),
+    plotOutput("p")
+  )
+),
+absolutePanel( 
+  wellPanel(
+    titlePanel("Draggable panel"),
+    "Move this panel anywhere you want.",
+  ),
+  varSelectInput("panelVar", "Select variable", mtcars),
+  width = "300px", 
+  right = "50px", 
+  top = "50px", 
+  draggable = TRUE, 
+), 
+navset_tab( 
+  nav_panel("A", "Page A content"), 
+  nav_panel("B", "Page B content"), 
+  nav_panel("C", "Page C content"), 
+  nav_menu( 
+    "Other links", 
+    nav_panel("D", "Panel D content"), 
+    "----", 
+    "Description:", 
+    nav_item( 
+      a("Shiny", href = "https://shiny.posit.co", target = "_blank") 
+    ), 
+  ), 
+), 
+id = "tab" 
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  # choose file output
+  
+  output$file1_contents <- renderPrint({print(input$file1)})
+  
+  # date range output
+  
+  # You can access the value of the widget with input$date
+  output$date <- renderPrint({ input$date })
+  output$date1 <- renderPrint({ input$date[[1]] })
+  output$date2 <- renderPrint({ input$date[[2]] })
+  
+  output$date1_class <- renderPrint({ class(input$date[[1]]) })
+  output$date1_year <- renderPrint({ year(input$date[[1]]) })
 
     output$distPlot <- renderPlot({
         # generate bins based on input$bins from ui.R
